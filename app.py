c3980767a64fd190f9acd032e25b16700c6c4f59
devopspocvgs <37547130+devopspocvgs@users.noreply.github.com>
2018-03-19 18:41:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import os
 import flask
-import MySQLdb
 
 application = flask.Flask(__name__)
 application.debug = True
 
-@application.route('/populate')
-def populate():
-  storage = Storage()
-  storage.populate()
-  score = storage.score()
-  return "Hello Devops this is staging version, %d!" % score
+@application.route('/sample-request')
+def sample_request():
+  return {"message":"Hello Devops this is staging version"}
 
 @application.route('/liveness')
 def liveness():
@@ -20,30 +16,6 @@ def liveness():
 @application.route('/health')
 def health():
   return "Healthy this is staging version"
-
-
-class Storage():
-  def __init__(self):
-    self.db = MySQLdb.connect(
-      user   = os.getenv('MYSQL_USERNAME'),
-      passwd = os.getenv('MYSQL_PASSWORD'),
-      db     = os.getenv('MYSQL_INSTANCE_NAME'),
-      host   = os.getenv('MYSQL_SERVICE_HOST'),
-      port   = int(os.getenv('MYSQL_SERVICE_PORT'))
-    )
-
-    cur = self.db.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS scores(score INT)")
-
-  def populate(self):
-    cur = self.db.cursor()
-    cur.execute("INSERT INTO scores(score) VALUES(1234)")
-
-  def score(self):
-    cur = self.db.cursor()
-    cur.execute("SELECT * FROM scores")
-    row = cur.fetchone()
-    return row[0]
 
 if __name__ == "__main__":
   application.run(host='0.0.0.0', port=3000)
