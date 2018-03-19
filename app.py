0d50d49953545d06c6b6a9a78ff687904fd7ba6b
devopspocvgs <37547130+devopspocvgs@users.noreply.github.com>
2018-03-19 18:09:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def populate():
   storage = Storage()
   storage.populate()
   score = storage.score()
-  return "Hello Devops 123, %d!" % score
+  return "Hello Devops this is production version, %d!" % score
 
 @application.route('/liveness')
 def liveness():
   storage = Storage()
-  return "Container is Alive"
+  return "Container is Alive this is production version"
 
 @application.route('/health')
 def health():
-  return "Healthy"
+  return "Healthy this is production version"
 
 
 class Storage():
